f5028150b8b41e4bc373ada00d3d321eb802907c
Ilia <chernoffilya1997@mail.ru>
2026-05-30 10:25:56 +0700

    change performance test

--- performance_test.py
+++ performance_test.py
@@ -7,34 +7,37 @@ from sort_by_color_mark import ColorSort, Item
 
 
 def test_perfomance():
-    """Выполнение сортировки 1000 маркированных элементов"""
-    # Пример: накопление в глобальном списке
-    marks = ['К', 'З', 'С']
-    objects = [Item(marks[random.randint(0, 2)], i) for i in range(1000)]
-    result = ColorSort.reorder(objects, 'К<С<З')
-    expected_list = []
-    for color in ('К', 'С', 'З'):
-        expected_list.extend([obj for obj in objects if obj.mark == color])
-    assert result == expected_list
+    def _func():
+        """Выполнение сортировки 1000 маркированных элементов"""
+        # Пример: накопление в глобальном списке
+        marks = ['К', 'З', 'С']
+        objects = [Item(marks[random.randint(0, 2)], i) for i in range(1000)]
+        result = ColorSort.reorder(objects, 'К<С<З')
+        expected_list = []
+        for color in ('К', 'С', 'З'):
+            expected_list.extend([obj for obj in objects if obj.mark == color])
+        assert result == expected_list
+
+    print("Выполняю функцию...")
+    start = time.perf_counter()
+    _func()
+    work_time = time.perf_counter() - start
+    print(f"Время выполнения: {work_time:.4f} секунд")
+    print("Функция выполнена.")
+    assert work_time < 1
 
 def main():
     pid = os.getpid()
     print(f"PID процесса: {pid} (для оценки памяти ps -p {pid} -o pid,rss,vsz)")
+    test_perfomance()
+
     print("Нажмите Enter, чтобы выполнить функцию...")
     input()
 
     # Запускаем трассировку памяти
     tracemalloc.start()
-    # Делаем первый снимок до вызова функции
+    # Делаем первый снимок до повторного вызова функции
     snapshot_before = tracemalloc.take_snapshot()
-
-    print("Выполняю функцию...")
-    start = time.perf_counter()
-    test_perfomance()
-    work_time = time.perf_counter() - start
-    print(f"Время выполнения: {work_time:.4f} секунд")
-    print("Функция выполнена.")
-    assert work_time < 1
 
     # Принудительно собираем мусор, чтобы временные объекты были удалены
     gc.collect()
